c22c4049c67af9de24246d73a7b924647438c9d9
Драгутин Гавриловић <55387611+NikolaTNT@users.noreply.github.com>
2023-05-09 01:47:36 +0200

    Add files via upload

---
+++ scripts/nebuladice.js
@@ -0,0 +1,8 @@
+Hooks.once('diceSoNiceInit', (dice3d) => {
+    dice3d.addTexture("Nebula", {
+        name: "Nebula",
+        composite: "source-over",
+        source: "images/nebula.webp",
+    });
+
+});
