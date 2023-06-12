9a33ababcd57696cf0d32afe0fef9b0e2cd14970
Драгутин Гавриловић <55387611+NikolaTNT@users.noreply.github.com>
2023-06-12 19:27:55 +0200

    Delete scripts directory

--- scripts/nebuladice.js
+++
@@ -1,8 +0,0 @@
-Hooks.once('diceSoNiceInit', (dice3d) => {
-    dice3d.addTexture("Nebula", {
-        name: "Nebula",
-        composite: "source-over",
-        source: "images/nebula.webp",
-    });
-
-});
